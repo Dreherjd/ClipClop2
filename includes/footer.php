8d0dc240445da14064f6a164b210e411dd6044f3
Justin Dreher <dreherjd71@gmail.com>
2024-06-21 16:38:08 -0400

    added footer, blank about page

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,10 @@
 </div>
+<footer>
+    <div class="container">
+        <h2 class="text-primary-emphasis">Clip Clop</h2>
+        <p>It's a blog about pony play</p>
+        <a class="text-primary-emphasis" href="<?php echo BASE_URL?>views/about.php">About</a>
+    </div>
+</footer>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -7,13 +7,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo BASE_URL?>assets/images/favicon-32x32.png">
+    <title>Cip Clop</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="<?php echo BASE_URL ?>index.php">Clip Clop</a>
+            <a class="navbar-brand" href="<?php echo BASE_URL ?>index.php"><img src="<?php echo BASE_URL?>assets/images/favicon-32x32.png" alt="A Horse"> Clip Clop</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
