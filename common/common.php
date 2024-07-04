edac849829aeb50dc6df8cabdc4179668f11aa6f
dreherjd <dreherjd71@gmail.com>
2024-07-04 13:52:33 -0400

    initial commit on linux VM

--- common/common.php
+++ common/common.php
@@ -14,11 +14,20 @@ function escapeHTML($html){
     return htmlspecialchars($html, ENT_HTML5, 'UTF-8');
 }
 
+/**
+ * Converts new lines entered in a textArea into ptags
+ * @param String - the text with new lines
+ * @return String - the text with p tags
+ */
 function convertNewLinesToParagraphs($text){
     $escaped = escapeHTML($text);
     return '<p>' . str_replace("\n", "</p><p>", $escaped) . '</p>';
 }
 
+/**
+ * redirects user to passed in path
+ * @param String - the path you wish to have the user redirected to
+ */
 function redirect($url){
     echo '<script language="javascript">window.location.href ="'.$url.'"</script>';
 }
